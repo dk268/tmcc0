c7f978d53acf47a02bcb0c413ad70aed1d3b2393
Danny <Lorddanp@aol.com>
2020-07-29 16:43:00 -0400

    Feat(many inputs): makes calculator accept an arbitrary number of inputs in its input

--- tmcc/src/playground/utilities.js
+++ tmcc/src/playground/utilities.js
@@ -24,6 +24,9 @@ const validateInput = input => {
 
   if (!operators.includes(inputArray[2])) return "invalid operator";
 
+  if (inputArray[2] === "/" && inputArray[1] === "0")
+    return "you are dividing by zero; stop it";
+
   return true;
 };
 
@@ -31,12 +34,29 @@ function add(n1, n2) {
   return n1 + n2;
 }
 
+function subtract(n1, n2) {
+  return n1 - n2;
+}
+
+function multiply(n1, n2) {
+  return n1 * n2;
+}
+
+function divide(n1, n2) {
+  return n1 / n2;
+}
+
 const operatorMap = {
   "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide,
 };
 
 const parseInput = input => {
   const splitArray = input.trim().split(" ");
+
+  if (splitArray.length > 3) return parseLonger(input);
 
   return [[splitArray[0], splitArray[1]].map(Number), splitArray[2]];
 };
@@ -47,4 +67,67 @@ const executeOperation = input => {
   return operatorMap[operator](...operands);
 };
 
-module.exports = { validateInput, executeOperation };
+const parseLonger = input => {
+  const splitArray = input.trim().split(" ");
+
+  // 3 3 + 3 3 * -
+
+  // const operands = splitArray.filter(token => !isNaN(token * 1));
+
+  function add(n1, n2) {
+    return n1 + n2;
+  }
+
+  function subtract(n1, n2) {
+    return n1 - n2;
+  }
+
+  function multiply(n1, n2) {
+    return n1 * n2;
+  }
+
+  function divide(n1, n2) {
+    return n1 / n2;
+  }
+
+  const operatorMap = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+  };
+
+  // const parseInput = input => {
+  //   const splitArray = input.trim().split(" ");
+
+  //   if (splitArray.length > 3) return parseLonger(input);
+
+  //   return [[splitArray[0], splitArray[1]].map(Number), splitArray[2]];
+  // };
+
+  // const executeOperation = input => {
+  //   const [operands, operator] = parseInput(input);
+
+  //   return operatorMap[operator](...operands);
+  // };
+
+  const operators = splitArray.filter(token => !!operatorMap[token]);
+
+  for (let i = 0; i < splitArray.length; i++) {
+    const current = splitArray[i];
+    console.log(splitArray);
+    if (operatorMap[current]) {
+      splitArray.splice(
+        i - 2,
+        3,
+        operatorMap[current](splitArray[i - 2] * 1, splitArray[i - 1] * 1)
+      );
+      operators.shift();
+      if (operators.length) i = 0;
+    }
+  }
+
+  return splitArray[0];
+};
+
+module.exports = { validateInput, executeOperation, parseLonger };
--- tmcc/src/playground/utilities.spec.js
+++ tmcc/src/playground/utilities.spec.js
@@ -1,6 +1,6 @@
 /* global describe, test */
 
-const { validateInput, executeOperation } = require("./utilities");
+const { validateInput, executeOperation, parseLonger } = require("./utilities");
 
 // describe("The utilities used by this application", () => {
 //   describe("include input validations", () => {
@@ -34,8 +34,29 @@ describe("Utilities with regard to postfix calculator", () => {
   });
 
   describe("the executor", () => {
-    test("correctly adds a properly formatted input string", () => {
+    test("correctly adds a properly formatted input string for addition", () => {
       expect(executeOperation("111 222 +")).toEqual(333);
+      expect(executeOperation("-1 -2 +")).toEqual(-3);
+    });
+    test("correctly adds a properly formatted input string for subtraction", () => {
+      expect(executeOperation("222 222 -")).toEqual(0);
+      expect(executeOperation("222 220 -")).toEqual(2);
+      expect(executeOperation("222 333 -")).toEqual(-111);
+      expect(executeOperation("-1 -2 -")).toEqual(1);
+    });
+    test("correctly adds a properly formatted input string for multiplication", () => {
+      expect(executeOperation("4 2 *")).toEqual(8);
+      expect(executeOperation("-1 4 *")).toEqual(-4);
+      expect(executeOperation("0 5756 *")).toEqual(0);
+    });
+    test("correctly adds a properly formatted input string for division", () => {
+      expect(executeOperation("111 222 /")).toEqual(0.5);
+      expect(executeOperation("-2 -1 /")).toEqual(2);
+      expect(executeOperation("400 0 /")).toEqual(Infinity);
+    });
+    test("correctly does longer inputs", () => {
+      expect(parseLonger("3 3 + 3 3 * -")).toBe(-3);
+      expect(parseLonger("3 3 + 3 3 * - 3 3 + 3 3 * -")).toBe(-3);
     });
   });
 });
